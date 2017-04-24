c7828e4e819a610b653e6d330e0d8081c25fc1d8
Wes T4N <wesleyt1998@gmail.com>
2017-04-23 21:30:37 -0400

    added header, foooter, function and php files. Setup main.js and scss files. Added index.php file

---
+++ includes/config.php
@@ -0,0 +1,17 @@
+<?php
+// ** MySQL connection settings ** //
+
+//Keep this as localhost for the course server
+define('DB_HOST', 'localhost');
+
+// Your course server username - sm68sp15
+define('DB_USER', 'wt237sp17');
+
+// Your course server password
+define('DB_PASSWORD', 'wesleyt0');
+
+// The name of the database to which you want to connect
+// info230_SP16_username - info230_SP16_sm68sp16
+define('DB_NAME', 'cafe_pacific_local');
+// define('DB_NAME', 'info230_sp17_wt237sp17');
+?>
---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Cafe Pacific | Ithaca NY</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="./css/style.css"/>
+
+  <script
+    src="https://code.jquery.com/jquery-3.1.1.min.js"
+    integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+    crossorigin="anonymous">
+  </script>
+  <script src="./js/main.js"></script>
+  <link href="./css/bootstrap.min.css" rel="stylesheet" />
+  <script src="https://use.fontawesome.com/104d9a8f78.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+  <?php require_once "./php/functions.php"; ?>
+</head>
+
+<body>
