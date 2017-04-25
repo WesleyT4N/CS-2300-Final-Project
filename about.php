2b91c7cc6f25fdb7c458d0c4a7c06e67adaf61c6
Wes T4N <wesleyt1998@gmail.com>
2017-04-24 23:57:16 -0400

    Added additional styling, and created translate buttons

--- about.php
+++ about.php
@@ -1,5 +1,6 @@
 <?php include('./includes/header.php'); ?>
 <div class="main-container">
   <h1 class="page-header-title">about</h1>
+  <button class="btn translate-button">eng / jpn</button>
 </div>
 <?php include('./includes/footer.php'); ?>
--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 <div class="banner-image">
 
 </div>
-<div class="main-container">
-  <h1 class="page-header-title">welcome</h1>
+<div class="main-container" id="welcomeContainer">
+  <div class="content-container">
+      <h1 class="page-header-title">welcome</h1>
+      <button class="btn translate-button">eng/jpn</button>
+  </div>
 </div>
+
 <?php include('./includes/footer.php'); ?>
