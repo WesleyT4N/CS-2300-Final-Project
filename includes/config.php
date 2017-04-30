35f2a15bc8dbdaa20d718dd753f4acb1b3450590
jachen1997 <jc2535@cornell.edu>
2017-04-30 15:20:45 -0400

    Add files via upload

--- includes/config.php
+++ includes/config.php
@@ -5,13 +5,16 @@
 define('DB_HOST', 'localhost');
 
 // Your course server username - sm68sp15
-define('DB_USER', 'wt237sp17');
+//define('DB_USER', 'wt237sp17');
+define('DB_USER', 'fp_supercool');
 
 // Your course server password
-define('DB_PASSWORD', 'wesleyt0');
+//define('DB_PASSWORD', 'wesleyt0');
+define('DB_PASSWORD', 'uWq_6y_8R7Zqy!NR');
 
 // The name of the database to which you want to connect
 // info230_SP16_username - info230_SP16_sm68sp16
-define('DB_NAME', 'cafe_pacific_local');
+//define('DB_NAME', 'cafe_pacific_local');
 // define('DB_NAME', 'info230_sp17_wt237sp17');
+define('DB_NAME', 'info230_SP17_fp_supercool');
 ?>
