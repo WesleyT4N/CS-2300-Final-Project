d2a637a7ac1da5e530c02d8669f6c023cebfa2df
Wes T4N <wesleyt1998@gmail.com>
2017-05-03 15:22:04 -0400

    fixed menu style

--- menu.php
+++ menu.php
@@ -9,229 +9,207 @@
         <button class="btn translate-button" id="menuTranslateBtn" type="button">eng / jpn</button>
       </div>
     </div>
-</div>
-    <?php
-    require_once "includes/config.php";
-    $mysqli = new mysqli( DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-    
-    if ($mysqli->connect_errno) {
-        echo "Failed";
-        echo "Errno: " . $mysqli->connect_errno . "\n";
-        echo "Error: " . $mysqli->connect_error . "\n";
-    }
-    ?>
+  </div>
+  <?php
+  require_once "includes/config.php";
+  $mysqli = new mysqli( DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+  if ($mysqli->connect_errno) {
+    echo "Failed";
+    echo "Errno: " . $mysqli->connect_errno . "\n";
+    echo "Error: " . $mysqli->connect_error . "\n";
+  }
+  ?>
   <div class="menu-container">
-    <div class="row menu-row">
-      <div class="col-sm-6">
-        <div class="item-type-container col-sm-12">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="item-type-container">
           <h1 class="menu-header">entrees</h1>
           <hr />
           <div class="item-container" id="entrees">
             <?php
-              $entrees = $mysqli->query("SELECT * FROM Items LEFT JOIN TypeOfItems ON Items.itemID = TypeOfItems.itemID WHERE TypeOfItems.typeID = 8");
-              while($row = $entrees->fetch_assoc()) {
-                  $pic = $row[ 'filePath' ];
-                  $credit = $row[ 'credit' ];
-                  $en = $row[ 'en' ];
-                  $jp = $row[ 'jp' ];
-                  $price = $row[ 'price' ];
-                  
-                  echo "<p>$en</p>";
-                  echo "<p>$price</p>";
-                  
-                  // if you want to use these later on
-                  //echo "<p>$pic</p>";
-                  //echo "<p>$credit</p>";
-                  //echo "<p>$jp</p>";
-              }
+            $entrees = $mysqli->query("SELECT * FROM Items LEFT JOIN TypeOfItems ON Items.itemID = TypeOfItems.itemID WHERE TypeOfItems.typeID = 8");
+            while($row = $entrees->fetch_assoc()) {
+              $pic = $row[ 'filePath' ];
+              $credit = $row[ 'credit' ];
+              $en = $row[ 'en' ];
+              $jp = $row[ 'jp' ];
+              $price = $row[ 'price' ];
+
+              echo "<p>$en <span style='float: right;'>$price</span></p>";
+
+              // if you want to use these later on
+              //echo "<p>$pic</p>";
+              //echo "<p>$credit</p>";
+              //echo "<p>$jp</p>";
+            }
             ?>
           </div>
         </div>
-      </div>
-      <div class="col-sm-6">
-        <div class="item-type-container col-sm-12">
+        <div class="item-type-container">
           <h1 class="menu-header">noodles</h1>
           <hr />
           <div class="item-container" id="noodles">
             <?php
-              $noodles = $mysqli->query("SELECT * FROM Items LEFT JOIN TypeOfItems ON Items.itemID = TypeOfItems.itemID WHERE TypeOfItems.typeID = 3");
-              while($row = $noodles->fetch_assoc()) {
-                  $pic = $row[ 'filePath' ];
-                  $credit = $row[ 'credit' ];
-                  $en = $row[ 'en' ];
-                  $jp = $row[ 'jp' ];
-                  $price = $row[ 'price' ];
-                  
-                  echo "<p>$en</p>";
-                  echo "<p>$price</p>";
-                  
-                  // if you want to use these later on
-                  //echo "<p>$pic</p>";
-                  //echo "<p>$credit</p>";
-                  //echo "<p>$jp</p>";
-              }
+            $noodles = $mysqli->query("SELECT * FROM Items LEFT JOIN TypeOfItems ON Items.itemID = TypeOfItems.itemID WHERE TypeOfItems.typeID = 3");
+            while($row = $noodles->fetch_assoc()) {
+              $pic = $row[ 'filePath' ];
+              $credit = $row[ 'credit' ];
+              $en = $row[ 'en' ];
+              $jp = $row[ 'jp' ];
+              $price = $row[ 'price' ];
+
+              echo "<p>$en <span style='float: right;'>$price</span></p>";
+
+              // if you want to use these later on
+              //echo "<p>$pic</p>";
+              //echo "<p>$credit</p>";
+              //echo "<p>$jp</p>";
+            }
             ?>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="row menu-row">
-      <div class="col-sm-6">
-        <div class="item-type-container col-sm-12">
+        <div class="item-type-container">
+          <h1 class="menu-header">sides</h1>
+          <hr />
+          <div class="item-container" id="sides">
+            <?php
+            $sides = $mysqli->query("SELECT * FROM Items LEFT JOIN TypeOfItems ON Items.itemID = TypeOfItems.itemID WHERE TypeOfItems.typeID = 4");
+            while($row = $sides->fetch_assoc()) {
+              $pic = $row[ 'filePath' ];
+              $credit = $row[ 'credit' ];
+              $en = $row[ 'en' ];
+              $jp = $row[ 'jp' ];
+              $price = $row[ 'price' ];
+
+              echo "<p>$en <span style='float: right;'>$price</span></p>";
+
+              // if you want to use these later on
+              //echo "<p>$pic</p>";
+              //echo "<p>$credit</p>";
+              //echo "<p>$jp</p>";
+            }
+            ?>
+          </div>
+        </div>
+        <div class="item-type-container">
           <h1 class="menu-header">salads</h1>
           <hr />
           <div class="item-container" id="salads">
             <?php
-              $salads = $mysqli->query("SELECT * FROM Items LEFT JOIN TypeOfItems ON Items.itemID = TypeOfItems.itemID WHERE TypeOfItems.typeID = 2");
-              while($row = $salads->fetch_assoc()) {
-                  $pic = $row[ 'filePath' ];
-                  $credit = $row[ 'credit' ];
-                  $en = $row[ 'en' ];
-                  $jp = $row[ 'jp' ];
-                  $price = $row[ 'price' ];
-                  
-                  echo "<p>$en</p>";
-                  echo "<p>$price</p>";
-                  
-                  // if you want to use these later on
-                  //echo "<p>$pic</p>";
-                  //echo "<p>$credit</p>";
-                  //echo "<p>$jp</p>";
-              }
+            $salads = $mysqli->query("SELECT * FROM Items LEFT JOIN TypeOfItems ON Items.itemID = TypeOfItems.itemID WHERE TypeOfItems.typeID = 2");
+            while($row = $salads->fetch_assoc()) {
+              $pic = $row[ 'filePath' ];
+              $credit = $row[ 'credit' ];
+              $en = $row[ 'en' ];
+              $jp = $row[ 'jp' ];
+              $price = $row[ 'price' ];
+
+              echo "<p>$en <span style='float: right;'>$price</span></p>";
+
+              // if you want to use these later on
+              //echo "<p>$pic</p>";
+              //echo "<p>$credit</p>";
+              //echo "<p>$jp</p>";
+            }
+            ?>
+          </div>
+        </div>
+
+        <div class="item-type-container">
+          <h1 class="menu-header">snacks</h1>
+          <hr />
+          <div class="item-container" id="snacks">
+            <?php
+            $snacks = $mysqli->query("SELECT * FROM Items LEFT JOIN TypeOfItems ON Items.itemID = TypeOfItems.itemID WHERE TypeOfItems.typeID = 6");
+            while($row = $snacks->fetch_assoc()) {
+              $pic = $row[ 'filePath' ];
+              $credit = $row[ 'credit' ];
+              $en = $row[ 'en' ];
+              $jp = $row[ 'jp' ];
+              $price = $row[ 'price' ];
+
+              echo "<p>$en <span style='float: right;'>$price</span></p>";
+
+              // if you want to use these later on
+              //echo "<p>$pic</p>";
+              //echo "<p>$credit</p>";
+              //echo "<p>$jp</p>";
+            }
             ?>
           </div>
         </div>
       </div>
-      <div class="col-sm-6">
-        <div class="item-type-container col-sm-12">
+
+      <div class="col-md-6">
+        <div class="item-type-container">
           <h1 class="menu-header">rice dishes</h1>
           <hr />
           <div class="item-container" id="riceDishes">
             <?php
             $rice = $mysqli->query("SELECT * FROM Items LEFT JOIN TypeOfItems ON Items.itemID = TypeOfItems.itemID WHERE TypeOfItems.typeID = 7");
-              while($row = $rice->fetch_assoc()) {
-                  $pic = $row[ 'filePath' ];
-                  $credit = $row[ 'credit' ];
-                  $en = $row[ 'en' ];
-                  $jp = $row[ 'jp' ];
-                  $price = $row[ 'price' ];
-                  
-                  echo "<p>$en</p>";
-                  echo "<p>$price</p>";
-                  
-                  // if you want to use these later on
-                  //echo "<p>$pic</p>";
-                  //echo "<p>$credit</p>";
-                  //echo "<p>$jp</p>";
-              }
+            while($row = $rice->fetch_assoc()) {
+              $pic = $row[ 'filePath' ];
+              $credit = $row[ 'credit' ];
+              $en = $row[ 'en' ];
+              $jp = $row[ 'jp' ];
+              $price = $row[ 'price' ];
+
+              echo "<p>$en <span style='float: right;'>$price</span></p>";
+
+              // if you want to use these later on
+              //echo "<p>$pic</p>";
+              //echo "<p>$credit</p>";
+              //echo "<p>$jp</p>";
+            }
             ?>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="row menu-row">
-      <div class="col-sm-6">
-        <div class="item-type-container col-sm-12">
-          <h1 class="menu-header">sides</h1>
-          <hr />
-          <div class="item-container" id="sides">
-            <?php
-              $sides = $mysqli->query("SELECT * FROM Items LEFT JOIN TypeOfItems ON Items.itemID = TypeOfItems.itemID WHERE TypeOfItems.typeID = 4");
-              while($row = $sides->fetch_assoc()) {
-                  $pic = $row[ 'filePath' ];
-                  $credit = $row[ 'credit' ];
-                  $en = $row[ 'en' ];
-                  $jp = $row[ 'jp' ];
-                  $price = $row[ 'price' ];
-                  
-                  echo "<p>$en</p>";
-                  echo "<p>$price</p>";
-                  
-                  // if you want to use these later on
-                  //echo "<p>$pic</p>";
-                  //echo "<p>$credit</p>";
-                  //echo "<p>$jp</p>";
-              }
-            ?>
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-6">
-        <div class="item-type-container col-sm-12">
-          <h1 class="menu-header">deserts</h1>
-          <hr />
-          <div class="item-container" id="deserts">
-            <?php
-              $desserts = $mysqli->query("SELECT * FROM Items LEFT JOIN TypeOfItems ON Items.itemID = TypeOfItems.itemID WHERE TypeOfItems.typeID = 5");
-              while($row = $desserts->fetch_assoc()) {
-                  $pic = $row[ 'filePath' ];
-                  $credit = $row[ 'credit' ];
-                  $en = $row[ 'en' ];
-                  $jp = $row[ 'jp' ];
-                  $price = $row[ 'price' ];
-                  
-                  echo "<p>$en</p>";
-                  echo "<p>$price</p>";
-                  
-                  // if you want to use these later on
-                  //echo "<p>$pic</p>";
-                  //echo "<p>$credit</p>";
-                  //echo "<p>$jp</p>";
-              }
-            ?>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row menu-row">
-      <div class="col-sm-6">
-        <div class="item-type-container col-sm-12">
+
+        <div class="item-type-container">
           <h1 class="menu-header">drinks</h1>
           <hr />
           <div class="item-container" id="drinks">
             <?php
-              $drinks = $mysqli->query("SELECT * FROM Items LEFT JOIN TypeOfItems ON Items.itemID = TypeOfItems.itemID WHERE TypeOfItems.typeID = 1");
-              while($row = $drinks->fetch_assoc()) {
-                  $pic = $row[ 'filePath' ];
-                  $credit = $row[ 'credit' ];
-                  $en = $row[ 'en' ];
-                  $jp = $row[ 'jp' ];
-                  $price = $row[ 'price' ];
-                  
-                  echo "<p>$en</p>";
-                  echo "<p>$price</p>";
-                  
-                  // if you want to use these later on
-                  //echo "<p>$pic</p>";
-                  //echo "<p>$credit</p>";
-                  echo "<p>Japanese name: $jp</p>";
-              }
+            $drinks = $mysqli->query("SELECT * FROM Items LEFT JOIN TypeOfItems ON Items.itemID = TypeOfItems.itemID WHERE TypeOfItems.typeID = 1");
+            while($row = $drinks->fetch_assoc()) {
+              $pic = $row[ 'filePath' ];
+              $credit = $row[ 'credit' ];
+              $en = $row[ 'en' ];
+              $jp = $row[ 'jp' ];
+              $price = $row[ 'price' ];
+
+              echo "<p>$en <span style='float: right;'>$price</span></p>";
+
+              // if you want to use these later on
+              //echo "<p>$pic</p>";
+              //echo "<p>$credit</p>";
+              // echo "<p>Japanese name: $jp</p>";
+            }
             ?>
           </div>
         </div>
-      </div>
-      <div class="col-sm-6">
-        <div class="item-type-container col-sm-12">
-          <h1 class="menu-header">snacks</h1>
+
+        <div class="item-type-container">
+          <h1 class="menu-header">deserts</h1>
           <hr />
-          <div class="item-container" id="snacks">
+          <div class="item-container" id="deserts">
             <?php
-              $snacks = $mysqli->query("SELECT * FROM Items LEFT JOIN TypeOfItems ON Items.itemID = TypeOfItems.itemID WHERE TypeOfItems.typeID = 6");
-              while($row = $snacks->fetch_assoc()) {
-                  $pic = $row[ 'filePath' ];
-                  $credit = $row[ 'credit' ];
-                  $en = $row[ 'en' ];
-                  $jp = $row[ 'jp' ];
-                  $price = $row[ 'price' ];
-                  
-                  echo "<p>$en</p>";
-                  echo "<p>$price</p>";
-                  
-                  // if you want to use these later on
-                  //echo "<p>$pic</p>";
-                  //echo "<p>$credit</p>";
-                  //echo "<p>$jp</p>";
-              }
+            $desserts = $mysqli->query("SELECT * FROM Items LEFT JOIN TypeOfItems ON Items.itemID = TypeOfItems.itemID WHERE TypeOfItems.typeID = 5");
+            while($row = $desserts->fetch_assoc()) {
+              $pic = $row[ 'filePath' ];
+              $credit = $row[ 'credit' ];
+              $en = $row[ 'en' ];
+              $jp = $row[ 'jp' ];
+              $price = $row[ 'price' ];
+
+              echo "<p>$en <span style='float: right;'>$price</span></p>";
+
+              // if you want to use these later on
+              //echo "<p>$pic</p>";
+              //echo "<p>$credit</p>";
+              //echo "<p>$jp</p>";
+            }
             ?>
           </div>
         </div>
