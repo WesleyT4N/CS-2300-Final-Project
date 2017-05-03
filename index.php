a1bac70427a0b606bc70316d577784f0494116e3
Wes T4N <wesleyt1998@gmail.com>
2017-05-02 23:42:17 -0400

    tweak to welcome message

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
     </div>
     <div class="row">
       <p class="welcome-text">
-        Come to Café Pacific, located in the heart of collegetown, for a cozy 
-        home-cooked Japanese meal and hearty dessert, or simply stop by for 
+        Come to Café Pacific, located in the heart of Collegetown, for a cozy
+        home-cooked Japanese meal and hearty dessert, or simply stop by for
         some bubble tea and choose from a variety of Japanese snacks.
         <!--Japanese:
           カレッジタウンのカフェパシフィックで、美味しい家庭料理が食べられます。色々なデザートやお菓子もありますから、ぜひ来てください！
