c534e4e4bdce2f29ca5335a4700680012e1128e9
jachen1997 <jc2535@cornell.edu>
2017-05-13 18:36:47 -0400

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,4 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="js/validate.js"></script>
+    </head>
 <?php include('./includes/header.php'); ?>
+
 <div class="main-container">
   <div class="container-fluid contact-container">
     <div class="row">
@@ -12,32 +18,54 @@
       </div>
     </div>
     <div class="contact-content">
-      <p id="phoneNumber">
-        phone (take-out)
-        <br />
-        <i class="fa fa-phone" aria-hidden="true"></i> (607)-277-7700
-      </p>
-      <hr />
-      <form id="contactForm" method="post">
-        <h2 id="commentHeader">leave a comment</h2>
+        <p id="phoneNumber">phone (take-out)<br />
+            <i class="fa fa-phone" aria-hidden="true"></i> (607)-277-7700
+        </p>
         <hr />
-        <div class="form-group">
-          <label>name</label>
-          <input id="name" autocomplete="off" type="text" name="name" placeholder="Commenter Name" />
-        </div>
-        <div class="form-group">
-          <label>comment</label>
-          <textarea id="comment" autocomplete="off" type="textarea" name="comment" placeholder="(comments, suggestions, etc.)" rows="4"></textarea>
-        </div>
-        <div class="form-group submit-container">
-          <button class="btn" type="button" id="commentSubmit" name="submit">send</button>
-        </div>
-      </form>
+        
+        <form action="contact.php" id="contactForm" method="post" onsubmit="return validateForm(this);">
+            <h2 id="commentHeader">leave a comment</h2>
+            <hr />
+            <div class="form-group">
+                <label>name</label>
+                <input id="name" autocomplete="off" type="text" name="name" placeholder="Commenter Name" /><div class="error"></div>
+            </div>
+            <div class="form-group">
+                <label>comment</label>
+                <textarea id="comment" autocomplete="off" type="textarea" name="comment" placeholder="(comments, suggestions, etc.)" rows="4"></textarea><div class="error"></div>
+            </div>
+            <div class="form-group submit-container">
+                <button class="btn" type="submit" id="submit" name="submit" class="submit">send</button><div class="error"></div>
+            </div>
+        </form>
+        
       <?php
+        
+        if (isset($_POST['name'])) {
+	       $name = $_POST['name'];
+        }
+        else {
+            $name = "";
+        }
+        
+        if (isset($_POST['comment'])) {
+	       $comment = $_POST['comment'];
+        }
+        else {
+            $comment = "";
+        }
+
+        if (isset($_POST['submit'])) {
+            $subject = "New comment from $name.";
+            mail('jachen1997@yahoo.com', $subject, $comment);
+            echo "Thank you for your feedback!";
+        }           
+           
       // PSEUDOCODE:
       // If the button is pressed:
 
         // Sanitize name and comment inputs. If any of the fields are empty raise an error
+        
 
         // Use mail() function to send to designated email ex: mail("placeholder@gmail.com", "subject", comment, headers)
       ?>
@@ -45,3 +73,4 @@
   </div>
 </div>
 <?php include('./includes/footer.php'); ?>
+</html>
