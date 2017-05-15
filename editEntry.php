20a2e38f25de7b7fe114ef6914ec68f092079c4b
jachen1997 <jc2535@cornell.edu>
2017-05-15 00:12:41 -0400

    Add files via upload

--- editEntry.php
+++ editEntry.php
@@ -12,6 +12,20 @@ if (isset($_POST['edit-row-id'])) {
     $english = filter_input( INPUT_POST, 'itemNameEn', FILTER_SANITIZE_STRING);
     $japanese = filter_input( INPUT_POST, 'itemNameJp', FILTER_SANITIZE_STRING);
     $price = filter_input( INPUT_POST, 'itemPrice', FILTER_SANITIZE_NUMBER_INT);
+    $blank = $mysqli->query("SELECT * FROM Items WHERE itemID = $rowToEdit");
+    $row = $blank->fetch_assoc();
+    $en = $row['en'];
+    $jp = $row['jp'];
+    $pr = $row['price'];
+    if ($english == "") {
+        $english = $en;
+    }
+    if ($japanese == "") {
+        $japanese = $jp;
+    }
+    if ($price == "") {
+        $price = $pr;
+    }
     $one = $mysqli->query("UPDATE Items SET en = '$english', jp = '$japanese', price = '$price' WHERE itemID = $rowToEdit");
         // Perform prepared statement database query
 
